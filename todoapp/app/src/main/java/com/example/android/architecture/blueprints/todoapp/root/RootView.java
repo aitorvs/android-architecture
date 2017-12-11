7d5f01eb2b18e607f35269be2f8a4f8a6b60d2e2
Aitor Viana <aitorvs@gmail.com>
2017-12-11 10:24:15 +0100

    Use butterknife to bind views.

--- todoapp/app/src/main/java/com/example/android/architecture/blueprints/todoapp/root/RootView.java
+++ todoapp/app/src/main/java/com/example/android/architecture/blueprints/todoapp/root/RootView.java
@@ -1,14 +1,21 @@
 package com.example.android.architecture.blueprints.todoapp.root;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.RelativeLayout;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import com.example.android.architecture.blueprints.todoapp.R;
 
 /**
  * Top level view for {@link RootBuilder.RootScope}.
  */
 class RootView extends RelativeLayout implements RootInteractor.RootPresenter {
+    @BindView(R.id.root_container) FrameLayout container;
 
     public RootView(Context context) {
         this(context, null);
@@ -20,5 +27,15 @@ class RootView extends RelativeLayout implements RootInteractor.RootPresenter {
 
     public RootView(Context context, @Nullable AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        ButterKnife.bind(this);
+    }
+
+    @NonNull public final ViewGroup viewContainer() {
+        return container;
     }
 }
