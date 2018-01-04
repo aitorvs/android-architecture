24835601c4370d8a0e30776526d0284ac28a3b1f
Aitor Viana <aitorvs@gmail.com>
2018-01-04 23:33:56 +0100

    Show snackbar when TODO title is empty rather than disabling the FAB

--- todoapp/app/src/main/java/com/example/android/architecture/blueprints/todoapp/root/task_flow/add_task/AddTaskView.java
+++ todoapp/app/src/main/java/com/example/android/architecture/blueprints/todoapp/root/task_flow/add_task/AddTaskView.java
@@ -3,6 +3,7 @@ package com.example.android.architecture.blueprints.todoapp.root.task_flow.add_t
 import android.content.Context;
 import android.support.annotation.Nullable;
 import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBar;
 import android.util.AttributeSet;
 import android.view.View;
@@ -15,11 +16,9 @@ import com.example.android.architecture.blueprints.todoapp.R;
 import com.example.android.architecture.blueprints.todoapp.data.Task;
 import com.example.android.architecture.blueprints.todoapp.util.Services;
 import com.example.android.architecture.blueprints.todoapp.util.Strings;
-import com.jakewharton.rxbinding2.widget.RxTextView;
 import com.jakewharton.rxrelay2.PublishRelay;
 import com.jakewharton.rxrelay2.Relay;
 import io.reactivex.Observable;
-import timber.log.Timber;
 
 /**
  * Top level view for {@link AddTaskBuilder.AddTaskScope}.
@@ -49,10 +48,6 @@ public class AddTaskView extends CoordinatorLayout implements AddTaskInteractor.
     protected void onFinishInflate() {
         super.onFinishInflate();
         ButterKnife.bind(this);
-        // title view observable
-        RxTextView.textChanges(title)
-            .map(input -> !Strings.isBlank(input.toString()))
-            .subscribe(hasInput -> doneButton.setEnabled(hasInput));
     }
 
     @Override
@@ -85,13 +80,20 @@ public class AddTaskView extends CoordinatorLayout implements AddTaskInteractor.
     }
 
     @OnClick(R.id.done_button) void done() {
-        Timber.d("done() called");
-        InputMethodManager imm = (InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
-        imm.hideSoftInputFromWindow(getWindowToken(), 0);
-        publishRelay.accept(new TaskViewModel(title.getText().toString(), description.getText().toString()));
+        if (isValidInput()) {
+            InputMethodManager imm = (InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(getWindowToken(), 0);
+            publishRelay.accept(new TaskViewModel(title.getText().toString(), description.getText().toString()));
+        } else {
+            Snackbar.make(AddTaskView.this, R.string.todos_cannot_be_empty, Snackbar.LENGTH_LONG).show();
+        }
     }
 
     private ActionBar getActionBar() {
         return Services.getSupportActionBar(getContext());
+    }
+
+    private boolean isValidInput() {
+        return !Strings.isBlank(title.getText().toString());
     }
 }
